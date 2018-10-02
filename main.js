9d79c9332dfe5d1ad46c786244b2dca3440680ca
Trinax44 <42937393+Trinax44@users.noreply.github.com>
2018-10-02 18:40:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+var Bot = new Discord.Client();
+var prefix = ("$");
+
+Bot.on('ready', () => {
+    Bot.user.setPresence({game: {name : '$help Tampax'}})
+    console.log("Bot Prêt !");
+});
+
+
+
+Bot.on('message', message => {
+    if (message.content === "bonjour"){
+        message.reply("Bonjour a Toi!");
+        console.log('bonjour Bonjour a Toi!');
+    }
+
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Voici Les commandes du Bot : \n Se Bot n'est la que pour souhaiter le bienvenu sur le serveur.");
+        console.log("Commande Help demandée !");
+    }
+
+});
+
+Bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+      return channel.send('Bienvenue sur le serveur Seikatsu' + member.displayName)
+    }).catch(console.error)
+    // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
+  })
+
+Bot.login('NDg2MTQ1NTM5OTk4NzQ0NTc3.DpD4LQ.gXnIupWO6tOD989fhqdRB4fO8es');
